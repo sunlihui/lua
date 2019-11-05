6332d65e273f04e59c21561ba65cad3dda782b5d
root <root@sh1-arch-slh-2.ad.tuhu.cn>
2019-11-05 15:58:17 +0800

    addd lua file

---
+++ data-type.lua
@@ -0,0 +1,17 @@
+-- 数据类型
+-- 6种数据类型
+--
+-- nil    表示不存在的空对象或者无效值，类似python的None
+-- boolean    布尔类型，取值为true或者false
+-- number    数字类型，不区分整数和浮点数
+-- string    字符串类型
+-- function    函数类型
+-- table    表类型，非常灵活的数据结构
+--
+print(type(nil))
+print(type(true))
+print(type(123))
+print(type(1.23))
+print(type('kevin'))
+print(type(print))
+print(type(table))
